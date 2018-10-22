29c7021da7b16ba52e2aef944e094769193b75e7
Marvin Piekarek <marpme@users.noreply.github.com>
2018-10-22 21:38:01 +0200

    Locking wallet automatically/manually (#812)
    * Locking wallet automatically/manually
    
    * Add Berkeley DB4 into path before autogen the verge repo
    
    * Travis fixes, remove bins & add logs
    
    * Error management when brew fails to find some packages
    
    * Adding another log
    
    * Fix comments
    
    * Adding moc as a requirement

--- building/mac/requirements.sh
+++ building/mac/requirements.sh
@@ -1,11 +1,21 @@
+echo 'REMOVE OLD BREW COMPONENTS'
 #brew install boost pkg-config
 brew uninstall qt5
 
+echo 'ADD REQUIRED BREW COMPONENTS'
 # Make sure we have the C libs we need
 brew install boost@1.60 pkg-config automake
 
+echo 'ADD ADDITIONAL QT BREW COMPONENT'
 # Install some deps
-brew install qt@5.5 protobuf miniupnpc openssl qrencode berkeley-db4
+brew install qt@5.5 
 
+echo 'ADD ADDITIONAL BREW COMPONENTS'
+# Install even more deps
+brew install protobuf miniupnpc openssl qrencode berkeley-db4
+
+brew install moc
+
+echo 'LINK BREW COMPONENTS'
 # Make sure our stuff is linked in our path
 brew link automake autoconf
